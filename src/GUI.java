a40a4dbc7e5fe0063f3e3a27977061a0f3682d1d
Seif <seif_hunterman@yahoo.com>
2020-06-03 03:53:32 +0200

    rootlocus

---
+++ src/GUI.java
@@ -0,0 +1,99 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.geom.*;
+
+public class GUI extends JPanel{
+    int mar=50;
+    RootLocus rl;
+
+    public GUI (RootLocus rl){
+        this.rl = rl;
+    }
+
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g1=(Graphics2D)g;
+        g1.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        int width=getWidth();
+        int height=getHeight();
+        g1.draw(new Line2D.Double(width/2 + width/8,mar,width/2 + width/8,height-mar));
+        g1.draw(new Line2D.Double(mar,height/2,width-mar,height/2));
+        g1.setPaint(Color.BLACK);
+        for (int i = 0; i < 4; i++){
+            g1.fill(new Ellipse2D.Double((width/2 + i*width/8)-2, height/2-2,4,4));
+            g1.drawString(String.valueOf(20*i - 20),(width/2 + i*width/8)-2,height/2-2);
+            g1.fill(new Ellipse2D.Double((width/2 - i*width/8)-2, height/2-2,4,4));
+            g1.drawString(String.valueOf(20*-i - 20),(width/2 - i*width/8)-2,height/2-2);
+            if ( i < 2){
+                g1.fill(new Ellipse2D.Double((width/2+width/8)-2, (height/2 + i*width/8)-2,4,4));
+                g1.drawString(String.valueOf(20*-i),(width/2 + width/8)-2,(height/2 + i*width/8)-2);
+                g1.fill(new Ellipse2D.Double((width/2+width/8)-2, (height/2 - i*width/8)-2,4,4));
+                g1.drawString(String.valueOf(20*i),(width/2 + width/8)-2,(height/2 - i*width/8)-2);
+
+            }
+        }
+
+
+        double[][] points = rl.getPoints();
+        g1.setPaint(Color.RED);
+        double scale = (width/8) / 20;
+        for (int i = 0 ; i < points.length - 1; i++){
+            g1.fill(new Ellipse2D.Double((width/2+width/8)+(points[i][0] * scale)-3,(height/2)+(points[i][1] * scale)-3 ,6,6));
+        }
+        g1.fill(new Ellipse2D.Double((width/2+width/8) +(points[5][0] * scale) -3,(height/2)-(points[5][1] * scale)-3,6,6));
+        g1.setPaint(Color.YELLOW);
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(points[points.length - 1][0] * scale)-4,(height/2)+(points[points.length - 1][1] * scale)-4 ,8,8));
+
+        int[] angles = rl.getAsymptotesAngles();
+        g1.setPaint(Color.BLACK);
+        double x1 = (width/2+width/8)+(points[points.length - 1][0] * scale);
+        double y1 = (height/2)+(points[points.length - 1][1] * scale);
+        for (int i = 0 ; i < angles.length; i++){
+            g1.setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0));
+            g1.drawLine((int)x1,(int)y1,(int) (x1 + 700 * Math.cos(angles[i] * Math.PI / 180)),(int)(y1 + 700 * Math.sin(angles[i] * Math.PI / 180)));
+
+        }
+        g1.setPaint(Color.BLUE);
+        g1.setStroke(new BasicStroke(2));
+        g1.drawLine((int)((width/2+width/8) +(points[0][0] * scale)),(int)((height/2)+(points[0][1] * scale)),(int)((width/2+width/8) +(points[1][0] * scale)),(int)((height/2)+(points[1][1] * scale)));
+
+        QuadCurve2D curve2D = new QuadCurve2D.Double();
+        curve2D.setCurve((width/2+width/8) + (points[5][0] * scale)+400,(height/2)+(points[5][1] * scale)+315,(width/2+width/8) + (points[4][0] * scale) - 510,(height/2)+(points[4][1] * scale),(width/2+width/8) + (points[5][0] * scale)+400,(height/2)-(points[5][1] * scale)-315);
+        g1.draw(curve2D);
+
+
+        curve2D = new QuadCurve2D.Double();
+        curve2D.setCurve((width/2+width/8) + (points[2][0] * scale),(height/2)+(points[2][1] * scale),0,1650,(width/2+width/8) + (points[6][0] * scale) -20000,(height/2)+(points[6][1] * scale)+20000);
+        g1.draw(curve2D);
+
+        curve2D = new QuadCurve2D.Double();
+        curve2D.setCurve((width/2+width/8) + (points[3][0] * scale),(height/2)+(points[3][1] * scale),0,-700,(width/2+width/8) + (points[6][0] * scale) -20000,(height/2)+(points[6][1] * scale)-20000);
+        g1.draw(curve2D);
+
+        g1.setPaint(Color.MAGENTA);
+
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-20*scale)-4,(height/2) + (0*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-1.77508467636853*scale)-4,(height/2) + (0*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-51.6124576618157*scale)-4,(height/2) + (12.3661331343658*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-51.6124576618157*scale)-4,(height/2) + (-12.3661331343658*scale) -4,8,8));
+
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-20*scale)-4,(height/2) + (0*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-1.77508467636853*scale)-4,(height/2) + (0*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-51.6124576618157*scale)-4,(height/2) + (12.3661331343658*scale) -4,8,8));
+        g1.fill(new Ellipse2D.Double((width/2+width/8)+(-51.6124576618157*scale)-4,(height/2) + (-12.3661331343658*scale) -4,8,8));
+
+
+    }
+
+    public static void main(String args[]){
+        JFrame frame =new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int[][] poles = {{0,0},{-25,0},{-50,10},{-50,-10}};
+        RootLocus rl = new RootLocus(poles);
+        rl.print();
+        frame.add(new GUI(rl));
+        frame.setBounds(0,0,Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+}
---
+++ src/RootLocus.java
@@ -0,0 +1,112 @@
+public class RootLocus {
+
+    private int[][] poles;
+    private int[] asymptotesAngles = new int[4];
+    private double centroid;
+    private int[] pointsRealAxis;
+    private double breakaway;
+    private double departureAngle;
+    private double pointImaginaryAxis;
+
+    public RootLocus(int[][] poles){
+        this.poles = poles;
+        anglesAsymptotes();
+        calculateCentroid();
+        realAxis();
+        breakawayPoint();
+        angleDeparture();
+        imaginaryAxis();
+    }
+
+    private void anglesAsymptotes(){
+        for (int i = 0 ; i < poles.length; i++){
+            asymptotesAngles[i] = (2*i+1)*180/poles.length;
+        }
+    }
+
+    private void calculateCentroid(){
+        int total = 0 ;
+        for (int i = 0 ; i < poles.length ; i++){
+            total += poles[i][0];
+        }
+        centroid =(double) total / (double) poles.length;
+    }
+
+    private void realAxis(){
+        int[] temp = new int[2];
+        int k = 0;
+        for (int i = 0 ; i < poles.length ; i++){
+            if (poles[i][1] == 0){
+                temp[k++] = poles[i][0];
+            }
+        }
+        pointsRealAxis = temp;
+    }
+
+    private void breakawayPoint(){
+        double[] rootsDerivative = {-9.150390137,-38.6401693,-45.95944056};
+        for (int i = 0 ; i < rootsDerivative.length; i++){
+            if (rootsDerivative[i] <= pointsRealAxis[0] && rootsDerivative[i] >= pointsRealAxis[1]){
+                breakaway = rootsDerivative[i];
+                break;
+            }
+            else if (rootsDerivative[i] >= pointsRealAxis[0] && rootsDerivative[i] <= pointsRealAxis[1]){
+                breakaway = rootsDerivative[i];
+                break;
+            }
+        }
+    }
+
+    private void angleDeparture(){
+        int [][] complex = new int[1][2];
+        for (int i = 0 ; i < poles.length ; i++){
+            if (poles[i][1] > 0){
+                complex[0] = poles[i];
+            }
+        }
+        double phi = -90.0;
+        for (int i = 0 ; i < poles.length ; i++){
+            if (poles[i][1] == 0){
+                double denomerator = (complex[0][0]*-1.0) - (poles[i][0]*-1.0);
+                double tana = Math.atan(complex[0][1] / denomerator) * 180 / Math.PI;
+                phi -= 180.0 - tana;
+            }
+        }
+        departureAngle = phi + 180.0;
+    }
+
+    private void imaginaryAxis(){
+        int s2 = 520;
+        pointImaginaryAxis = Math.sqrt(s2);
+    }
+
+    public double[][] getPoints(){
+        double[][] points = new double[poles.length+3][2];
+        int i = 0;
+        for (int j = 0 ; j < poles.length; j++){
+            points[i][0] = poles[j][0];
+            points[i][1] = poles[j][1];
+            i++;
+        }
+        points[i][0] = breakaway;
+        points[i][1] = 0;
+        points[i+1][0] = 0;
+        points[i+1][1] = pointImaginaryAxis;
+        points[i+2][0] = centroid;
+        points[i+2][1] = 0;
+        return points;
+    }
+
+    public int[] getAsymptotesAngles() {
+        return asymptotesAngles;
+    }
+
+    public void print(){
+        System.out.println("The Poles are " + poles[0][0] + ", " + poles[1][0] + ", " + poles[2][0] + " ± " + poles[2][1] + "i");
+        System.out.println("Angles of Asymptotes are " + asymptotesAngles[0] + ", " + asymptotesAngles[1] + ", " + asymptotesAngles[2] + ", " + asymptotesAngles[3]);
+        System.out.println("The Centroid is " + centroid);
+        System.out.println("Breakaway point is " + breakaway);
+        System.out.println("Angle of Departure is " + departureAngle);
+        System.out.println("Intersect with imaginary axis in ±" + pointImaginaryAxis + "i");
+    }
+}
